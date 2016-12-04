a397cdeb1b1b56e113a95a88fb58fe4e71d48070
Melissa Garcia <mgarcia7@bu.edu>
2016-12-04 05:03:26 -0500

    added annotated sequences

--- genepredictor.py
+++ genepredictor.py
@@ -4,6 +4,7 @@
 # OUTPUT: Name of gene of all the conserved regions/likelihood that it is that gene
 ################################################################
 import numpy as np
+import re
 
 class PositionState:
 	# Match emission probability
@@ -31,7 +32,6 @@ class pHMM:
 	# emission probabilities by position
 
 	def __init__(self, sequences):
-		self.sequences = sequences
 		self.transition = create_transition(sequences)
 		self.emission = create_emission(sequences)
 
@@ -42,7 +42,82 @@ class pHMM:
 		pass
 
 
-def 
+class GeneSequence:
+	def __init__(self, iterator, genome):
+		self.sequence = ""
+		self.gene_name = None
+		self.location = None
+		self.genome = genome
+
+		for match in iterator:
+			text = match.group()
+			if "gene=" in text:
+				gene_name = text[5:]
+			if "location=" in text:
+				location = re.sub('[^0-9]\.','', a) #get rid of non-numeric and non-periods
+				location = tuple(text.split(".."))
+
+
+def read_in_annotated(filename, p): # p is the compiled pattern object
+	# Read in line by line, save sequence, check every 5 if the 5th one is dmDA
+		# if not, clear list and start over
+		# if so, save list and do the next 5 lines after dmDa
+	current_gene_sequence = None
+	gene_sequences = []
+
+	with open(filename,'r') as f:
+		count = 0
+		for line in f:
+			if ">lcl" in line and "gene=" in line: # start of new annotated
+				iterator = p.finditer(line)
+				current_gene_sequence = GeneSequence(iterator,filename.split("_")[0])
+
+			else if ">lcl" in line and "gene=" not in line:
+				current_gene_sequence = None
+
+			if current_gene_sequence is not None:
+				current_gene_sequence.sequence += line
+				
+
+
+
+	# Return a list of the genes surrounding dmDa for a genome
+
+	pass
+
+def create_gene_dict(genome_sequences)
+	# input = list of all sequences
+	# sort them by gene name and add them to a dict
+	# output = a dict where the key is the name of gene and the val is a list of all the gene sequences
+	pass
+
+def create_pHMMs(gene_sequences):
+	# input = dict from above function
+	# for each key-val pair, align the sequences and create a pHMM
+	# output = dict where name of gene is the key, and the val is the pHMM
+	pass
+
+def score_sequence(region_seq):
+	# input = conserved region
+	# get a score using every pHMM
+	# output = a dict with gene name as key, and val is the score
+	pass
+
+
+def create_unsorted_gene_list():
+	path = "data/annotations/"
+	filenames = ["Dshibae_annotated.txt", "Oantarcticus307_annotated.txt", "Oarcticus238_annotated.txt", "Rmobilis_annotated.txt", "Rpomeroyi_annotated.txt"]
+	unsorted_gene_list = []
+
+	p = re.compile("(?<=\[).+?(?=\])") #regex expression to match things in brackets
+	# read all files in annotation folder
+	for file in filenames:
+		gene_list = read_in_annotated(path+file,p)
+		unsorted_gene_list.append(gene_list)
+
+	# get total gene list to worry about
+	# output = unsorted gene list
+	return unsorted_gene_list
 
 # Create profile HMM model of a gene
 	# list - gene = [seq1, seq2, seq3]
